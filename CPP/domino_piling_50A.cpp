82ff4bdc5adbb17a308ecaede15dd256bfbe9af2
Akshat Dalal <63205271+AKSHATDALAL842@users.noreply.github.com>
2024-11-12 23:53:21 +0530

    50A

--- CPP/domino_piling_50A.cpp
+++ CPP/domino_piling_50A.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 
 int main(){
-    std::cout<<"Enter the dimensions of the board: ";
+    int m,n,total;
+    std::cin >> m >> n;
+    if(n%2==0){
+        n/=2;
+        total = n*m;
+        std::cout<<total;
+    } else {
+        n--;
+        n/=2;
+        if(m%2==0){
+            total = (n*m) + (m/2);
+            std::cout<<total;
+        } else {
+            total = (n*m) + ((m-1)/2);
+            std::cout<<total;
+        }
+    }
+
+    return 0;
 }
